b5bba05d274490f7d9928bce457a5995a17f9440
Akash Sharma <129882041+KumarSharmaAkash@users.noreply.github.com>
2024-08-20 20:36:36 -0700

    Update Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -30,7 +30,7 @@ const Profile = () => {
 
             <div className="mx-auto max-w-180">
               <h4 className="font-semibold text-black dark:text-white">
-              Simulating the mortality rate of toxic gases over a specified time period in collaboration with DRDO scientists. 
+              Simulating the mortality rate of toxic gases over a specified time period in collaboration with INMAS(DRDO) scientists. 
               </h4>
               <p className="mt-4.5">
               The development of this React.js-based website for calculating mortality rates due to toxic gas inhalation represents a significant step forward in addressing public health and safety concerns. By leveraging modern web technologies, we can enhance our understanding and response to the dangers posed by toxic gases.
